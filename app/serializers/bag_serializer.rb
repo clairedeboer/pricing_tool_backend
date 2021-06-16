5d63be5b196ee8a28a9727b261e303be5477cc66
clairedeboer <claire.deboer@gmail.com>
2021-06-16 18:08:12 -0400

    save and render photos

--- app/serializers/bag_serializer.rb
+++ app/serializers/bag_serializer.rb
@@ -1,3 +1,5 @@
 class BagSerializer < ActiveModel::Serializer
-  attributes :id, :designer, :style, :size, :material, :color, :condition, :retail_price, :resale_price, :featured_image
+  attributes :id, :designer, :style, :size, :material, :color, :condition, :retail_price, :resale_value
+
+  has_many :photos, serializer: PhotoSerializer
 end
--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -2,7 +2,7 @@ class PhotoSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :bag_id, :featured_image
 
-  def file
+  def featured_image
     if object.featured_image.attached?
       {
         url: rails_blob_url(object.featured_image)
